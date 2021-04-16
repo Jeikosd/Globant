f1b92ecac627c5595507e47fd737fa5118ed5081
Jeikosd <jeison.mesa@correounivalle.edu.co>
2021-04-16 15:17:10 -0500

    generating all cleaning data

--- merge_data.R
+++ merge_data.R
@@ -5,6 +5,10 @@ gc(reset = T)
 library(readr)
 library(tibble)
 library(stringr)
+library(dplyr)
+library(glue)
+
+
 purchase_transactions <- list.files(path = "data/cleaning/", pattern = "PCT", full.names = T) %>% 
   tibble(file_path = .) %>%
   mutate(file = basename(file_path),
@@ -17,4 +21,30 @@ purchase_transactions <- list.files(path = "data/cleaning/", pattern = "PCT", fu
   })) %>% 
   pull(data) %>% 
   bind_rows()
+
+## Make a daily sequence to identify 
+init_last <- purchase_transactions %>% 
+  filter(row_number()==1 | row_number()==n()) %>% 
+  dplyr::select(`TRANS DATE`) %>% 
+  pull()
+
+sequence_date <- seq.Date(from = init_last[1], to = init_last[2], by = "1 day") %>% 
+  tibble(date = .)
+
+all_data <- full_join(sequence_date, purchase_transactions, by = c("date" = "TRANS DATE"))
+
+write_csv(all_data, file = glue::glue("data/cleaning/sequence_purchase_transactions.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
