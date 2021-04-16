f56b2fe84218408cc52616a98d4e12c856ffffbf
Jeikosd <jeison.mesa@correounivalle.edu.co>
2021-04-15 22:22:44 -0500

    change system locale spanish to english"

--- cleaning_data.R
+++ cleaning_data.R
@@ -5,7 +5,10 @@ library(rvest)
 library(stringr)
 library(purrr)
 library(glue)
-
+library(open)
+# "Spanish_Colombia.1252"
+Sys.setlocale("LC_TIME", "English")
+# dowload files
 
 webpage_url <- "https://data.birmingham.gov.uk/dataset/purchase-card-transactions"
 webpage <- rvest::read_html(webpage_url)
@@ -30,3 +33,41 @@ purrr::map(.x = purchase_card_2, .f = function(x){
   download.file(x, glue::glue("data/{names_}"), mode = "wb")
 })
 
+# cleaning name files
+# cut-off date on the third day
+
+library(readxl)
+library(lubridate)
+datos <- read_xls("data/cusersfinainmndesktoppublish-copy-january-2017.xls")
+datos <- read_xls("data/cusersfinainmndesktoppublish-spend-april-2017.xls")
+datos <- datos %>% 
+  mutate(year = lubridate::year(`TRANS DATE`), 
+         month = lubridate::month(`TRANS DATE`),
+         day = lubridate::day(`TRANS DATE`),
+         wday = lubridate::wday(`TRANS DATE`, label = T, abbr = F))
+datos %>% 
+  dplyr::filter(year == 2016) %>% 
+  as.data.frame()
+
+datos %>% 
+  dplyr::filter(month == 2) %>% 
+  as.data.frame()
+
+datos %>% 
+  dplyr::select(year, month) %>% 
+  distinct(year, month)
+
+datos %>% 
+  dplyr::select(year, month, day, wday) %>% 
+  distinct(year, month, day, wday) %>% 
+  dplyr::filter(month == 2)
+
+# purchase card transition
+## Change to PCT_month_year
+# Example PCT_01_2016  -> purchase card transition january 2016
+datos <- read_xls("data/cusersfinainmndesktoppublish-copy-january-2017.xls")
+datos <- datos %>% 
+  mutate(year = lubridate::year(`TRANS DATE`), 
+         month = lubridate::month(`TRANS DATE`),
+         day = lubridate::day(`TRANS DATE`),
+         wday = lubridate::wday(`TRANS DATE`, label = T))
