e3918bdd39bb6367fe874bb235a80a2d63388e5b
Jeikosd <jeison.mesa@correounivalle.edu.co>
2021-04-15 23:41:04 -0500

    cleaning names (xls) to PCT_year_month

--- cleaning_data.R
+++ cleaning_data.R
@@ -7,9 +7,11 @@ library(purrr)
 library(glue)
 library(readxl)
 library(lubridate)
+library(readr)
 
 ## dir to save the purchase card transactions
 fs::dir_create("data/")
+fs::dir_create("data/cleaning")
 
 # "Spanish_Colombia.1252" my locale
 Sys.setlocale("LC_TIME", "English")
@@ -39,8 +41,36 @@ purrr::map(.x = purchase_card_2, .f = function(x){
 })
 
 # cleaning name files
-# cut-off date on the third day
+# purchase card transactions
+## Change to PCT_month_year
+# Example PCT_01_2016  -> purchase card transition january 2016
+cleaning_name <- function(x, out_dir){
+  
+  datos <- read_xls("data/cusersfinainmndesktoppublish-copy-january-2017.xls")
+  datos <- read_xls(x)
+  
+  datos <- datos %>% 
+    mutate(year = lubridate::year(`TRANS DATE`), 
+           month = lubridate::month(`TRANS DATE`),
+           day = lubridate::day(`TRANS DATE`),
+           wday = lubridate::wday(`TRANS DATE`, label = T))
+  
+  # Selecting the month with the most data to generate the file name
+  name_purchase_card <- datos %>% 
+    dplyr::count(year, month) %>% 
+    top_n(1, n) %>% 
+    mutate(month = sprintf("%.2d", month),
+           name_purchase_card = glue::glue("PCT_{year}_{month}.csv")) %>% 
+    pull(name_purchase_card)
+  
+  write_csv(x = datos, file = glue::glue("{out_dir}/{name_purchase_card}"))
+  
+}
+xls_files <- list.files(path = "data/", pattern = "*.xls", full.names = T)
+purrr::map(.x = xls_files, .f = cleaning_name, out_dir = "data/cleaning/")
 
+
+# cut-off date on the third day
 
 datos <- read_xls("data/cusersfinainmndesktoppublish-copy-january-2017.xls")
 datos <- read_xls("data/cusersfinainmndesktoppublish-spend-april-2017.xls")
@@ -66,12 +96,4 @@ datos %>%
   distinct(year, month, day, wday) %>% 
   dplyr::filter(month == 2)
 
-# purchase card transactions
-## Change to PCT_month_year
-# Example PCT_01_2016  -> purchase card transition january 2016
-datos <- read_xls("data/cusersfinainmndesktoppublish-copy-january-2017.xls")
-datos <- datos %>% 
-  mutate(year = lubridate::year(`TRANS DATE`), 
-         month = lubridate::month(`TRANS DATE`),
-         day = lubridate::day(`TRANS DATE`),
-         wday = lubridate::wday(`TRANS DATE`, label = T))
+
